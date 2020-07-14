90f5b69293ccb8996ef72f634e37d796a0f06b2f
Davys lima <davysjunior08@hotmail.com>
2020-07-14 19:42:11 -0300

    Estilização Dashboard, finalizada.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
 
 import logoImg from "../../assets/Logo.svg";
 
-import { Title, Form } from "./styles";
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,45 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/25330254?s=460&u=6eeb6a1dfb19e57679bcd816b2e899fc49cd15a5&v=4"
+            alt="Davys Lima"
+          />
+          <div>
+            <strong>dunseen/OSPMPA</strong>
+            <p>A web system responsive using React.JS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/25330254?s=460&u=6eeb6a1dfb19e57679bcd816b2e899fc49cd15a5&v=4"
+            alt="Davys Lima"
+          />
+          <div>
+            <strong>dunseen/OSPMPA</strong>
+            <p>A web system responsive using React.JS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/25330254?s=460&u=6eeb6a1dfb19e57679bcd816b2e899fc49cd15a5&v=4"
+            alt="Davys Lima"
+          />
+          <div>
+            <strong>dunseen/OSPMPA</strong>
+            <p>A web system responsive using React.JS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { shade } from "polished";
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -21,6 +22,11 @@ export const Form = styled.form`
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
   }
 
   button {
@@ -31,9 +37,61 @@ export const Form = styled.form`
     border: 0;
     color: #fff;
     font-weight: bold;
+    transition: background-color 0.3s;
 
     &:hover {
-      background: #04d200;
+      background: ${shade(0.2, "#04d361")};
     }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 55px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+    transition: transform 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+  }
+
+  img {
+    width: 64px;
+    height: 64px;
+    border-radius: 50%;
+  }
+
+  div {
+    margin-left: 16px;
+
+    strong {
+      font-size: 20px;
+      color: #3d3d4d;
+    }
+
+    p {
+      font-size: 18px;
+      color: #a8a8b3;
+      margin-top: 4px;
+    }
+  }
+  svg {
+    margin-left: auto;
+    color: #cbcbd6;
   }
 `;
